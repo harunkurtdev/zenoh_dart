f63fee7b66137d75d5ed03831269df999b2c4ac0
harunkurtdev <harunkurt.dev@gmail.com>
2025-10-23 20:17:59 +0300

    include file add

---
+++ src/include/zenoh_configure.h
@@ -0,0 +1,9 @@
+#pragma once
+#define ZENOH_C "1.6.2"
+#define ZENOH_C_MAJOR 1
+#define ZENOH_C_MINOR 6
+#define ZENOH_C_PATCH 2
+#define ZENOH_C_TWEAK
+
+#define TARGET_ARCH_AARCH64
+#define Z_FEATURE_TRANSPORT_WS
